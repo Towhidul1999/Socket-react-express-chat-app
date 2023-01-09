43f1297cdfa8b8e92b8de562ee1c3dac62e3f48b
Towhidul <towhidulsislam@gmail.com>
2023-01-09 16:59:16 +0600

    socket react express project

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const http = require('http')
+const expressServer = http.createServer(app)
+const { Server } = require('socket.io')
+const io = new Server(expressServer)
+const path = require('path')
+
+app.use(express.static('client/build'))
+
+app.get('*', (req, res) => {
+    res.sendFiles(path.resolve(__dirname, 'client', 'build', 'index.html'))
+})
+
+app.get('/express-server', (req, res) => {
+    res.send('This is my bakend')
+})
+
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    setTimeout(function () {
+        socket.emit('msg', 'This is message from server')
+    }, 2000)
+
+    socket.on('disconnect', () => {
+        console.log('User Disconnected')
+    })
+})
+
+expressServer.listen(5000, () => {
+    console.log('Server listening on port: 5000')
+})
